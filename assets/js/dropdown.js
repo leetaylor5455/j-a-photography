6d57ace5076cbd5208793c5109554c6dd28296e9
Lee Taylor <leetaylor5455@yahoo.co.uk>
2021-05-14 19:57:24 +0100

    1.0 Finish

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,6 +1,10 @@
 $(function() {
 
     $('.dropdown-parent').on('click', function(e) {
+        if (e.target.tagName === 'A') { // if link clicked, dont close lists
+            return;
+        }
+
         $dropdownParent = $(this);
         $dropdownChild = $(this).find('.dropdown-child');
         $dropdownIcon = $(this).find('.dropdown-icon');
@@ -9,11 +13,9 @@ $(function() {
         $dropdownIcon.addClass('up');
 
         if ($dropdownParent.hasClass('down')) {
-
-            e.preventDefault();
             $dropdownChild.css('height', $dropdownChild.children().length * 30 + 'px');
 
-        } else if ($(e.target).hasClass('dropdown-icon')) {
+        } else {
 
             $dropdownChild.css('height', '0px');
             $dropdownParent.removeClass('down');
